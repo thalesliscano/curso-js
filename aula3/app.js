d95776e092016a139444e4e7f51ebdc17d1a8e33
thales <thalesdliscano@gmail.com>
2024-02-07 06:14:45 -0400

    add-aula3

---
+++ aula3/app.js
@@ -0,0 +1,8 @@
+"use strick";
+
+let num1 = Number(prompt("digite um numero"));
+let num2 = Number(prompt("digite um numero"));
+
+const somaDosNumero = num1 + num2;
+
+alert(`A soma dos número é ${somaDosNumero}`);
