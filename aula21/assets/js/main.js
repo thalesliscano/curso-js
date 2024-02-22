871bcb418fe76745801594b69ca4759958e442f2
thales <thalesdliscano@gmail.com>
2024-02-22 07:52:35 -0400

    add-aula21-aplicando-dicas

--- aula21/assets/js/main.js
+++ aula21/assets/js/main.js
@@ -7,7 +7,28 @@ const btns = container.querySelector(".controles");
 const start = btns.querySelector("#start");
 const stoped = btns.querySelector("#stop");
 const restart = btns.querySelector("#restart");
+
+document.addEventListener("click", function (event) {
+  const el = event.target;
+  if (el.id === "start") {
+    clearInterval(intervalo);
+    intervalo = setInterval(() => {
+      seconds++;
+      relogio.textContent = convertHorario(seconds);
+    }, 10);
+    relogio.style.color = "#0f0";
+  } else if (el.id === "stop") {
+    clearInterval(intervalo);
+    relogio.style.color = "#f00";
+  } else if (el.id === "restart") {
+    clearInterval(intervalo);
+    relogio.textContent = "00:00:00";
+    relogio.style.color = "#000";
+  }
+});
+
 let seconds = 0;
+
 let intervalo;
 const convertHorario = (segundos) => {
   const data = new Date(segundos * 1000);
@@ -16,23 +37,3 @@ const convertHorario = (segundos) => {
     timeZone: "GMT",
   });
 };
-
-start.addEventListener("click", () => {
-  clearInterval(intervalo);
-  intervalo = setInterval(() => {
-    seconds++;
-    relogio.textContent = convertHorario(seconds);
-  }, 10);
-  relogio.style.color = "#0f0";
-});
-
-stoped.addEventListener("click", () => {
-  clearInterval(intervalo);
-  relogio.style.color = "#f00";
-});
-
-restart.addEventListener("click", () => {
-  clearInterval(intervalo);
-  relogio.textContent = "00:00:00";
-  relogio.style.color = "#000";
-});
